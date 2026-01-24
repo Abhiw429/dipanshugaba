92f0d96435fc06a01f2f89ee6ffff47fe4f7c063
Abhiw429 <63968824+Abhiw429@users.noreply.github.com>
2026-01-24 11:38:10 +0530

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,7 +2,7 @@ export default function AboutPage() {
   return (
     <section>
       <h1 className="text-3xl font-bold">About</h1>
-      <p>Artist bio here.</p>
+      <p> DG ARTIST.</p>
     </section>
   );
 }
