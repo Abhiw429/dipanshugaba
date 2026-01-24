fd5f6d8a61d8b0bf5591e063e53366fe84ff634e
Abhiw429 <63968824+Abhiw429@users.noreply.github.com>
2026-01-24 11:39:45 +0530

    Update page.tsx

--- app/music/camouflage-freeverse/page.tsx
+++ app/music/camouflage-freeverse/page.tsx
@@ -3,7 +3,95 @@ export default function SongPage() {
     <section className="space-y-4">
       <h1 className="text-2xl font-bold">Camouflage (Freeverse)</h1>
       <p>By Dipanshu Gaba</p>
-      <p>Lyrics page.</p>
+      <p>Prabal mhtav ho ya detya kann
+Satya meva jatya mann sashst singh
+Kast mastak keen nar
+Moostkeem jb rhete
+Karm kannthastya, bolein nar
+Kartvaya mev jatya
+
+Yamuna behti jahan se
+Ye ladka rhta wahaan pe
+Jhaaag mein jb hai behti laaashein
+Dhulke pohoche darkinnare
+Paap na dhulte jbtak chalti saanse
+Aur Banti baatein jabtak sala sanki naache
+Duniya sath h - chamke halki mannn ki baat dhann ke maarg se
+Bacche mein jo tansen plte jb ho bomb drop maaang pe pistil
+no sindoor shit
+
+Lacking loha paanch se 4 fifth loaded
+Caught it, Takin L inverted whole week
+Physc! this that cycle neither landing nor of the tulips
+Jb they are in their ladies sangeet justinthat case they are trippin
+Hoelland, wind-mills, trippy maula jbbhi
+They be seekin in my mastisk
+They-sees jhola bharke punches, they-sees hilti nhi h pistil, toh noo key-pinn (keepin) Donald piercing
+
+Jb kaam pe aake R dabbe
+Toh naam ke aage R lagge
+The one (Dhawan) who kinda motivates
+Aur kal ko aakr naam jappe
+Jo lac-tose intolarent uske muh pr aakr dhaaar lagge
+Saakaar kree vyaapaar jo dakshin - na guru ka maan krre
+
+Toh daaye baaye sb
+DishaAo mein, dekh
+Ghirraaa hu mai kali fake
+Nigghaaho se, seke hath
+Chittaon se, I mean the ash trays ahh
+Kiddo pr pest spray, girre matt grey - matt pe
+Red tape Boxed up Champagne for the next docs
+THE KID GROWS, gotsnub nose
+
+Charrrreeeee
+
+Callme! shitaan cuz jaldi ka kaam shaitaan ka
+Aur shubh kaam mein deri kaisi
+Shaitaani mehngi pdti jb sangat waisi
+Barkat ke langar lgte
+Ghar ghar jb khusiyaan aati
+Sar katkar harkat hoti
+Tb bhi milti maafi yah
+
+Girre-girre-Girre karvat lete hi, put up takiye 2,3
+Teri khatiya choti
+Teri Old school beat pe old school shit nhi
+Dakiyanoosi ahh
+
+Yahaan Mera Feature jaise carbonation
+Teri Saadi shit yaha fizzy bnne
+Tera of creature gya hibernation pe jb bars se mausam brfilla pddee
+
+Par teri bi*ch ho na thandi
+Oo mennu kehndi, dekho hath pe mehndi
+Dhoondo naam kiska - aapka h ji
+Lekin Chup mai kyu hu, kyu inse dur hu?
+Cuz apne circle se meetho ka circle nikala
+That shit no dalgona candy. Woah
+
+There is no camouflage shit, no hidding
+Duniya se, ghaas bhoos, ke ahh peeche
+Bio student the only time I need lines when I write (biological name) krr
+
+Solanum Lycopersicum glut cuz sauce bohot when the mak-sad drops
+Ragde bhaang ke podho ko can I? bss kya hi lena
+The pitch goes down the slope nelumbo nucifera
+Lords kh do, kala hrraaa de sfed ko that's noo vitis vinifera
+Diye bohot saare good flows, ya god flows jb o zubaan pr h logo ki
+Todi clavicle tedhe garibaaan ftti tie bhi h bohoto ki
+
+But Ab sille kon
+Overused bar used hnn silly kon
+Pumped out chest bahar haan silicon
+Aaya Fresh maal seal h kon todde haayymannn bolle Ki
+Abhi toh shuru mera catalog
+
+Matha phod apne metaphod pr khata khol
+Kalam tod aur naam likh siyaahi se korre panno par
+Jaahaan iss khel ke top 4 love bohot
+To those ones who make songs and got some respect through this
+But...</p>
     </section>
   );
 }
