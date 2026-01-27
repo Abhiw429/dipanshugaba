981145bbc6d5a0e69a8306b6f956168e1132ad45
Abhiw429 <63968824+Abhiw429@users.noreply.github.com>
2026-01-27 22:43:17 +0530

    Update page.tsx

--- app/music/projects/[projectSlug]/[songSlug]/page.tsx
+++ app/music/projects/[projectSlug]/[songSlug]/page.tsx
@@ -1,17 +1,18 @@
 import { getSongBySlug } from "../../../../../lib/contentful";
 import SongClient from "./SongClient";
 
-export default async function Page() {
-  const res = await contentfulClient.getEntries({
-    content_type: "song",
-    "fields.slug": "camouflage-freeverse",
-    limit: 1,
-  });
+type PageProps = {
+  params: {
+    projectSlug: string;
+    songSlug: string;
+  };
+};
 
-  const song = res.items[0]?.fields;
+export default async function Page({ params }: PageProps) {
+  const song = await getSongBySlug(params.songSlug);
 
   if (!song) {
-    return <div className="max-w-5xl mx-auto p-10">Song not found</div>;
+    return <div className="p-10">Song not found</div>;
   }
 
   return <SongClient song={song} />;
