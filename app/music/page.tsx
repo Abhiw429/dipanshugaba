d489277939e26f357d936802ec429399a95ab26c
Abhiw429 <63968824+Abhiw429@users.noreply.github.com>
2026-01-28 12:57:54 +0530

    Update page.tsx

--- app/music/page.tsx
+++ app/music/page.tsx
@@ -6,6 +6,7 @@ import MusicClient from "./MusicClient";
 
 export const dynamic = "force-dynamic";
 
+
 export default async function MusicPage() {
   try {
     /* ================= LATEST (GLOBAL) ================= */
